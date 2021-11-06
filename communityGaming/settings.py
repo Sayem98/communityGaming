caa288aa87c4658cf47d6494ef58223e79e7606c
Sayem98 <sayemabedin.bd@gmail.com>
2021-11-06 09:22:45 +0600

    Hosted in heroku..

--- communityGaming/settings.py
+++ communityGaming/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
-# SECRET_KEY = 'django-insecure-s-h*7v=ac-ctv#$p(6yivg@d@-6v^!+i6@g%f5vh$wy9(k@oa+'
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
